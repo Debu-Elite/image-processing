9ba2296c62c0065d0091e93d79ad11e14033af07
debu <gautamdevansh183@gmail.com>
2022-12-20 13:21:28 +0530

    Increasing readability
    Signed-off-by: debu <gautamdevansh183@gmail.com>

--- negative.c
+++ negative.c
@@ -1,27 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 int main()
 {
+
     FILE *streamIn;// making a file pointer
     streamIn = fopen("lena512.bmp", "rb");// pointing the image file using file pointer
-    printf(" address before seek = %u\n",*streamIn);// printing the file address
+
     unsigned char imageHeader[54]; // to get the image header
     fread(imageHeader, sizeof(unsigned char), 54, streamIn);// reading the image header using fread function
     /*
-
     Following is the declaration for fread() function.
-
     size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
-
-
     ptr − This is the pointer to a block of memory with a minimum size of size*nmemb bytes.
-
     size − This is the size in bytes of each element to be read.
-
-     nmemb − This is the number of elements, each one with a size of size bytes.
-
-     stream − This is the pointer to a FILE object that specifies an input stream.
+    nmemb − This is the number of elements, each one with a size of size bytes.
+    stream − This is the pointer to a FILE object that specifies an input stream.
     */
+
+
+
+
+
 
     // extract image height and width from imageHeader
     int width = *(int*)&imageHeader[18];
@@ -34,30 +34,49 @@ int main()
 
     if(offset != 54)
     {
-
     // colore table is present
     printf("color is present\n");
     fseek( streamIn, 54, SEEK_SET);
-        printf("address after 54 bytes= %u \n",*streamIn);
 		fread(colorTable, sizeof(unsigned char), 1024, streamIn);// reading color table and storing it in colortable array
-		//printf(" bitDepth=%d ",bitDepth);
     }
+
+
+
+
+
 
    	// image data reading operations
 	int imgDataSize = width * height; // calculate image size
 
     unsigned char imageData[imgDataSize], newimageData[imgDataSize];// array to store the data of image
-    fread( imageData, sizeof(unsigned char), imgDataSize, streamIn);
+    fread( imageData, sizeof(unsigned char), imgDataSize, streamIn);// putting the data into imageData array
+
+
+
+
 
     for(int i=0;i<=imgDataSize;i++)
-    newimageData[i] = 255 - imageData[i];
-    FILE *fo = fopen("lena_negative.bmp", "wb");
+    newimageData[i] = 255 - imageData[i];// reversing the data of image
+
+
+
+
+    FILE *fo = fopen("lena_negative.bmp", "wb");// making a new image file and opening it in write mode
 
     fwrite(imageHeader, sizeof(unsigned char), 54, fo); // write the header back.
+    /*
+    The C library function size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
+    writes data from the array pointed to, by ptr to the given stream.
+    ptr − This is the pointer to the array of elements to be written.
+    size − This is the size in bytes of each element to be written.
+    nmemb − This is the number of elements, each one with a size of size bytes.
+    stream − This is the pointer to a FILE object that specifies an output stream.
+    */
     fwrite(colorTable, sizeof(unsigned char), 1024, fo); // write the color table back
     fwrite( newimageData, sizeof(unsigned char), imgDataSize, fo); // write the values of the negative image.
 
 fclose(fo);
 fclose(streamIn);
+
 
 }
